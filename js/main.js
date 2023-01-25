23525ecf22562bf261d55ef913c55ae44457efeb
VincenzoTavassi <info@egadivacanze.it>
2023-01-25 15:28:09 +0100

    completo e stile inserito

--- js/main.js
+++ js/main.js
@@ -5,30 +5,40 @@
 
 // CREO DIV PER CONTENERE RISULTATO 
 let DivContenitoreEl = document.createElement('div');
-// INSERISCO DIV NEL BODY 
-document.body.append(DivContenitoreEl);
-console.log(DivContenitoreEl);
+// ASSEGNO CLASSE ROW AL DIV 
+DivContenitoreEl.classList.add('row');
 
-// CICLO 
+// OTTENGO IL CONTAINER DELLA PAGINA
+let containerEl = document.getElementById('main-container')
+// INSERISCO DIV NEL CONTAINER
+containerEl.append(DivContenitoreEl);
+
+// CICLO DI 100 A PARTIRE DA 1
 for (i = 1; i < 100; i++) {
 
-    // CREO UN TAG P PER OGNI ELEMENTO
-    let numeroEl = document.createElement('p');
+    // CREO UN TAG DIV PER OGNI ELEMENTO E GLI ASSEGNO CLASSE BOX
+    let numeroEl = document.createElement('div');
+    numeroEl.classList.add('box');
 
-    // INSERISCO TUTTI I TAG P NEL DIV PRINCIPALE
+    // INSERISCO TUTTI I TAG DIV NEL DIV PRINCIPALE
     DivContenitoreEl.append(numeroEl);
 
-    // SE IL NUMERO E' MULTIPLO DI 5 E DI 3 STAMPO FIZZBUZZ
+    // SE IL NUMERO E' MULTIPLO DI 5 E DI 3 STAMPO FIZZBUZZ E ASSEGNO LA CLASSE ADATTA
     if (i % 3 == 0 && i % 5 == 0) {
-        numeroEl.innerHTML = 'FIZZBUZZ'
+        numeroEl.innerHTML = 'FIZZBUZZ';
+        numeroEl.classList.add('fizz-buzz');
+
     }
-    // ALTRIMENTI SE IL NUMERO E' MULTIPLO DI 3 STAMPO FIZZ
+    // ALTRIMENTI SE IL NUMERO E' MULTIPLO DI 3 STAMPO FIZZ E ASSEGNO LA CLASSE ADATTA
     else if (i % 3 == 0) {
-        numeroEl.innerHTML = 'fizz'
+        numeroEl.innerHTML = 'fizz';
+        numeroEl.classList.add('fizz');
     }
-    // ALTRIMENTI SE IL NUMERO E' MULTIPLO DI 5 STAMPO BUZZ 
+    // ALTRIMENTI SE IL NUMERO E' MULTIPLO DI 5 STAMPO BUZZ E ASSEGNO LA CLASSE
     else if (i % 5 == 0) {
-        numeroEl.innerHTML = 'buzz'
+        numeroEl.innerHTML = 'buzz';
+        numeroEl.classList.add('buzz');
+
     }
     // ALTRIMENTI STAMPO IL NUMERO STANDARD
     else {
